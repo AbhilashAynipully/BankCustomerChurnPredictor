b024dccbff3e58c91a86330f79bc3b871358864a
Abhilash Aynipully <abhidigitally@gmail.com>
2024-02-22 22:27:34 +0000

    Update Business Reuirements

--- app_pages/page1_summary.py
+++ app_pages/page1_summary.py
@@ -10,20 +10,20 @@ def page1_summary_body():
     # Project Terms and Jargons
     st.info(
         f"**Project Terms & Jargon**\n"
-        f"* A **customer** is an individual who uses banking services(bank account, credit card etc).\n"
+        f"* A **customer** is an individual who uses banking services(bank account, credit card, etc).\n"
         f"* An **exited customer** is a user who has stopped using banking services.\n"
-        f"* A **credit score** talks about the credit worthiness of the individual.\n "
+        f"* A **credit score** talks about the creditworthiness of the individual.\n "
         f"* This customer has a **tenure** level, the number of years this person " 
         f"has used our product/service.\n\n")
 
     st.info(
         f"**Project Dataset**\n"
         f"* The dataset contains information on **bank customers** who either left or "
-        f"continue to be a customer. There are details regarding:"
-        f"* Customer profile, like age, gender, surname, salary, credit score and geography."
-        f"* Customer information, like how many products they hold with bank,"
-        f" the number of years they have been wih bank, customer's account balance, " 
-        f" whether customer is active etc.")
+        f"continue to be a customer. There are details regarding the"
+        f"* Customer profile, like age, gender, surname, salary, credit score, and geography."
+        f"* Customer information, like how many products they hold with the bank,"
+        f" the number of years they have been with the bank, customer's account balance, " 
+        f" whether the customer is active, etc.")
    
 
     # Link to README file
@@ -38,6 +38,6 @@ def page1_summary_body():
         f"The project has **2 business requirements:**\n"
         f"* 1 - The bank is interested in identifying from the available data, the most "
         f"relevant customer attributes which are correlated to customer exit.\n\n"
-        f"* 2 - The bank is also interested in predicting, if a newly onboarded customer " 
-        f"would exit. If yes, when will it be."
+        f"* 2 - The bank is also interested in predicting if a newly onboarded customer " 
+        f"would exit. If yes, when will it be?"
         )
